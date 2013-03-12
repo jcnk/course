3951f64589617516189e4d4fe7683ab311c720bf
Jacek <jacek.cegiel@nasza-klasa.pl>
2013-03-12 11:57:18 +0100

    Create project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+   
+  
+  echo $_SERVER['REDIRECT_QUERY_STRING'];
+  echo PHP_EOL;
+  
+  
+  echo $_SERVER['QUERY_STRING'];
+  echo PHP_EOL;
+  
+  echo $_GET['w'];
+  echo PHP_EOL;
+  
+  var_dump($_SERVER);
+  
+?>
+
+
+
+
